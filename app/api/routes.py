ce69ddff2bfe87e960efe34f21912ebc80c3baea
Zach Todd <zchtodd@gmail.com>
2019-07-21 17:34:56 -0400

    frontend talking some ajax

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,8 +1,10 @@
+import json
+
 from flask import request
 from app.api import api_blueprint
 
 
 @api_blueprint.route("/execute/", methods=["POST"])
 def index():
-    data = requets.get_json()
-    return ""
+    data = request.get_json()
+    return json.dumps({"a":1})
