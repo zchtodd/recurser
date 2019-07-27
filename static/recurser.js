c18a325e9002861e4ec1fc885027d49acabe8b55
Zach Todd <zchtodd@gmail.com>
2019-07-27 15:59:47 -0400

    kind of working

--- static/recurser.js
+++ static/recurser.js
@@ -1,14 +1,14 @@
-let width = 1000;
-let height = 400;
+let margin = { top: 100, right: 20, bottom: 100, left: 20 };
+let width = 1000 - margin.left - margin.right;
+let height = 400 - margin.top - margin.bottom;
 
 let NODE_SIZE = 30;
-let NODE_SEP = NODE_SIZE * 2 + 10;
 
 class Node {
     constructor(x, y, prevSibling) {
         this.x = x;
         this.y = y;
-        this.finalX = 0;
+        this.finalY = 0;
         this.modifier = 0;
 
         this.prevSibling = prevSibling;
@@ -22,31 +22,31 @@ function calculateInitialValues(node) {
     }
 
     if (node.prevSibling) {
-        node.x = node.prevSibling.x + 3;
+        node.y = node.prevSibling.y + 3;
     } else {
-        node.x = 0;
+        node.y = 0;
     }
 
     if (node.children.length == 1) {
-        node.modifier = node.x;
+        node.modifier = node.y;
     } else if (node.children.length >= 2) {
-        let totalX = 0;
+        let totalY = 0;
         for (let i = 0; i < node.children.length; i++) {
-            totalX += node.children[i].x;
+            totalY += node.children[i].y;
         }
-        node.modifier = node.x - totalX / 2;
+        node.modifier = node.y - totalY / 2;
     }
 }
 
 function calculateFinalValues(node, modSum) {
-    node.finalX = node.x + modSum;
+    node.finalY = node.y + modSum;
     for (let i = 0; i < node.children.length; i++) {
         calculateFinalValues(node.children[i], node.modifier + modSum);
     }
 }
 
 function buildTree(dataNode, prevSibling, level) {
-    let root = new Node(0, level, prevSibling);
+    let root = new Node(level, 0, prevSibling);
     for (let i = 0; i < dataNode.children.length; i++) {
         root.children.push(
             buildTree(
@@ -64,27 +64,27 @@ function getDimensions(root) {
     let maxWidth = -minWidth;
 
     let minHeight = Infinity;
-    let maxHeight= -minWidth;
+    let maxHeight = -minWidth;
 
     let nodes = [root];
     while (nodes.length) {
         let node = nodes.shift();
         nodes = nodes.concat(node.children);
 
-        if (node.finalX < minWidth) {
-            minWidth = node.finalX;
+        if (node.x < minWidth) {
+            minWidth = node.x;
         }
 
-        if (node.finalX > maxWidth) {
-            maxWidth = node.finalX;
+        if (node.x > maxWidth) {
+            maxWidth = node.x;
         }
 
-        if (node.y < minHeight) {
-            minHeight = node.y;
+        if (node.finalY < minHeight) {
+            minHeight = node.finalY;
         }
 
-        if (node.y > maxHeight) {
-            maxHeight = node.y;
+        if (node.finalY > maxHeight) {
+            maxHeight = node.finalY;
         }
     }
     return [maxWidth - minWidth, maxHeight - minHeight];
@@ -96,41 +96,56 @@ function drawTree(svg, data) {
     calculateInitialValues(root);
     calculateFinalValues(root, 0);
 
-    let [treeHeight, treeWidth] = getDimensions(root);
+    //let g = document.createElementNS("http://www.w3.org/2000/svg", "g");
+    //g.setAttribute(
+    //    "transform",
+    //    `translate(${margin.left}, ${margin.top}) rotate(-90)`
+    //);
+
+    let [treeWidth, treeHeight] = getDimensions(root);
     let levelWidth = width / (treeWidth + 1);
     let levelHeight = height / (treeHeight + 1);
 
     NODE_SIZE = Math.min(NODE_SIZE, levelWidth, levelHeight);
+    let nodeOffsetX = levelWidth / 2 - NODE_SIZE / 2;
+    let nodeOffsetY = levelHeight / 2 - NODE_SIZE / 2;
 
     let nodes = [root];
     while (nodes.length) {
-        let node = nodes.shift();    
-        
-        /*
+        let node = nodes.shift();
+        let circ = document.createElementNS(
+            "http://www.w3.org/2000/svg",
+            "circle"
+        );
+
+        let x1 = node.x * levelWidth + nodeOffsetX;
+        let y1 = node.finalY * levelHeight + nodeOffsetY;
+
         for (let i = 0; i < node.children.length; i++) {
-            let line = document.createElementNS("http://www.w3.org/2000/svg", "line");
-            line.setAttribute("x1", node.finalX * NODE_SEP);
-            line.setAttribute("y1", node.y * NODE_SEP);
-            line.setAttribute("x2", node.children[i].finalX * NODE_SEP);
-            line.setAttribute("y2", node.children[i].y * NODE_SEP);
+            let x2 = node.children[i].x * levelWidth + nodeOffsetX;
+            let y2 = node.children[i].finalY * levelHeight + nodeOffsetY;
+
+            let line = document.createElementNS(
+                "http://www.w3.org/2000/svg",
+                "line"
+            );
+
+            line.setAttribute("x1", x1);
+            line.setAttribute("y1", y1);
+            line.setAttribute("x2", x2);
+            line.setAttribute("y2", y2);
             line.setAttribute("stroke", "red");
 
-    		svg.appendChild(line);
+            svg.appendChild(line);
         }
-        */
-
 
         nodes = nodes.concat(node.children);
 
-        let circ = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-        let x = node.finalX * levelHeight + levelHeight / 2 - NODE_SIZE / 2;
-        let y = node.y * levelWidth + levelWidth / 2 - NODE_SIZE / 2;
+        circ.setAttribute("cx", x1);
+        circ.setAttribute("cy", y1);
+        circ.setAttribute("fill", "red");
+        circ.setAttribute("r", NODE_SIZE);
 
-		circ.setAttribute("cx", x);
-		circ.setAttribute("cy", y);
-		circ.setAttribute("fill", "red");
-		circ.setAttribute("r", NODE_SIZE);
-
-		svg.appendChild(circ);
+        svg.appendChild(circ);
     }
 }
