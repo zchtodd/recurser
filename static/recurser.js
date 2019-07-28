e89bcbfa7f49e9daa55d1026572bb5a2f8b2d01c
Zach Todd <zchtodd@gmail.com>
2019-07-28 10:50:15 -0400

    fix node conflicts in tree

--- static/recurser.js
+++ static/recurser.js
@@ -50,11 +50,13 @@ function calculateInitialValues(node) {
     if (node.children.length == 1) {
         node.modifier = node.y;
     } else if (node.children.length >= 2) {
-        let totalY = 0;
+        let minY = Infinity;
+        let maxY = -minY;
         for (let i = 0; i < node.children.length; i++) {
-            totalY += node.children[i].y;
+            minY = Math.min(minY, node.children[i].y);
+            maxY = Math.max(maxY, node.children[i].y);
         }
-        node.modifier = node.y - totalY / 2;
+        node.modifier = node.y - (maxY - minY) / 2;
     }
 }
 
@@ -63,6 +65,43 @@ function calculateFinalValues(node, modSum) {
     for (let i = 0; i < node.children.length; i++) {
         calculateFinalValues(node.children[i], node.modifier + modSum);
     }
+}
+
+function getContour(root, val, func) {
+    let nodes = [root];
+    while (nodes.length) {
+        let node = nodes.shift();
+        nodes = nodes.concat(node.children);
+        val = func(val, node.finalY);
+    }
+    return val;
+}
+
+function shiftDown(root, shiftValue) {
+    let nodes = [root];
+    while (nodes.length) {
+        let node = nodes.shift();
+        nodes = nodes.concat(node.children);
+        node.finalY += shiftValue;
+    }
+}
+
+function fixNodeConflicts(root) {
+    for (let i = 0; i < root.children.length; i++) {
+        fixNodeConflicts(root.children[i]);
+    }
+
+    for (let i = 0; i < root.children.length - 1; i++) {
+        // Get the bottom-most contour position of the current node
+        let botContour = getContour(root.children[i], -Infinity, Math.max);
+
+        // Get the topmost contour position of the node underneath the current one
+        let topContour = getContour(root.children[i + 1], Infinity, Math.min);
+
+        if (botContour >= topContour) {
+            shiftDown(root.children[i + 1], botContour - topContour + 3);
+        }
+    } 
 }
 
 function buildTree(dataNode, prevSibling, level) {
@@ -77,6 +116,25 @@ function buildTree(dataNode, prevSibling, level) {
         );
     }
     return root;
+}
+
+function updateYVals(root) {
+    let minYVal = Infinity;
+    let nodes = [root];
+    while (nodes.length) {
+        let node = nodes.shift();
+        nodes = nodes.concat(node.children);
+        if (node.finalY < minYVal) {
+            minYVal = node.finalY;
+        }
+    }
+
+    nodes = [root];
+    while (nodes.length) {
+        let node = nodes.shift();
+        nodes = nodes.concat(node.children);
+        node.finalY += Math.abs(minYVal);
+    }
 }
 
 function getDimensions(root) {
@@ -115,6 +173,8 @@ function drawTree(svg, data) {
 
     calculateInitialValues(root);
     calculateFinalValues(root, 0);
+    updateYVals(root);
+    fixNodeConflicts(root);
 
     let existingG = svg.querySelector("g");
     if (existingG) {
@@ -134,10 +194,6 @@ function drawTree(svg, data) {
     let nodes = [root];
     while (nodes.length) {
         let node = nodes.shift();
-        let circ = document.createElementNS(
-            "http://www.w3.org/2000/svg",
-            "circle"
-        );
 
         let x1 = node.x * levelWidth + nodeOffsetX;
         let y1 = node.finalY * levelHeight + nodeOffsetY;
@@ -162,12 +218,28 @@ function drawTree(svg, data) {
 
         nodes = nodes.concat(node.children);
 
+        let circ = document.createElementNS(
+            "http://www.w3.org/2000/svg",
+            "circle"
+        );
+
         circ.setAttribute("cx", x1);
         circ.setAttribute("cy", y1);
         circ.setAttribute("fill", "steelblue");
         circ.setAttribute("r", NODE_SIZE);
 
         g.appendChild(circ);
+
+        let text = document.createElementNS(
+            "http://www.w3.org/2000/svg",
+            "text"
+        );
+
+        text.setAttribute("x", x1);
+        text.setAttribute("y", y1);
+        text.textContent = node.finalY;
+
+        g.appendChild(text);
     }
     svg.appendChild(g);
 }
